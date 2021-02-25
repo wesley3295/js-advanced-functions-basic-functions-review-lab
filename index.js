d8ac23e05bcb872f5155a5111d94aab8d3d630ba
Wesley <wspencer3295@yahoo.com>
2021-02-25 12:35:02 -0700

    commit tree 172dc98e544f0543243cdd0fb784e825e5284b65

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+function saturdayFun(firstParam = "roller-skate"){
+    return `This Saturday, I want to ${firstParam}!`
+}
+
+function mondayWork(firstParam="go to the office"){
+    return `This Monday, I will ${firstParam}.`
+}
+
+function wrapAdjective(style="*"){
+    return function(secondFnParam = "special"){
+      return  `You are ${style}${secondFnParam}${style}!`
+    }
+}
+
+const Calculator = {
+     add: function(firstNum,secondNum){
+            return firstNum + secondNum
+        },
+    subtract: function(firstNum,secondNum){
+            return firstNum - secondNum
+        },
+    multiply: function(firstNum,secondNum){
+            return firstNum * secondNum
+    },
+    divide: function(firstNum,secondNum){
+            return firstNum / secondNum
+    },
+    
+    
+}
+function actionApplyer(start,ray){
+
+    for (let i=0; i < ray.length; i++ ) {
+     start = ray[i](start )
+}
+return start 
+}
